b19a200ef5e615321ec90dd3195d4f8c7cc04a5b
sonuqazi <sonudeoria786@gmail.com>
2025-04-05 12:15:48 +0530

    Update Categories.php

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -1,1 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categories extends Model
+{
+    use HasFactory;
+    protected $table = 'categories';
+    protected $fillable = ['title', 'created_by'];
+
+    /**
+     * Relationship between Categories and Project model    
+     * @return object
+     */
+    public function projects() {
+        return $this->hasMany(Project::class, 'categories_id', 'id');
+    }
+}
 
